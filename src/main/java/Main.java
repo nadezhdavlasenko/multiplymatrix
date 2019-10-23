95ac60436cac1154245e07ff046199b2070c9898
Nadiia Vlasenko <nadia.vlasenko.mail@gmail.com>
2019-10-23 11:07:43 +0300

    Algorithm seems as working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -11,15 +10,69 @@ public class Main {
     System.out.println("Enter matrix size: ");
     try {
       int size = Integer.parseInt(reader.readLine());
-      List<List<Boolean>> matrix1 = new ArrayList<List<Boolean>>();
-      for (int i = 0; i < size; i++) {
-        matrix1.add(new ArrayList<Boolean>());
-        for (int j = 0; j < size; j ++) {
-          matrix1.get(i).add(new Random().nextBoolean());
-        }
-      }
+      BooleanMatrix matrix1 = new BooleanMatrix(size);
+      matrix1.populateRamdomly();
+      System.out.println(matrix1);
+      BooleanMatrix matrix2 = new BooleanMatrix(size);
+      matrix2.populateRamdomly();
+      System.out.println(matrix2);
+      BooleanMatrix result = matrix1.multiply(matrix2);
+      System.out.println(result);
+
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+}
+
+class BooleanMatrix {
+
+  private boolean[][] matrix;
+
+  BooleanMatrix(int size) {
+    this.matrix = new boolean[size][size];
+  }
+
+  public BooleanMatrix multiply(BooleanMatrix other) {
+    BooleanMatrix resultMatrix = new BooleanMatrix(size());
+    for (int i = 0; i < size(); i ++) {
+      for (int j = 0; j < size(); j ++) {
+        boolean sum = false;
+        for (int k = 0; k < size(); k++) {
+          sum = sum ^ (this.get(i, k) && other.get(k, j));
+        }
+        resultMatrix.matrix[i][j] =  sum;
+      }
+    }
+    return resultMatrix;
+  }
+
+  public boolean get(int i, int j) {
+    return matrix[i][j];
+  }
+
+  public int size() {
+    return matrix.length;
+  }
+
+
+  public void populateRamdomly() {
+    for (int i = 0; i < size(); i++) {
+      for (int j = 0; j < size(); j ++) {
+        matrix[i][j] = new Random().nextBoolean();
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+
+    String string = "BooleanMatrix{" + "matrix=\n";
+
+    for (int i = 0; i < size(); i ++ ) {
+      string+= Arrays.toString(matrix[i])+ "\n";
+    }
+    string+= '}';
+    return string;
   }
 }
